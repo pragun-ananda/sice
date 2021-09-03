750c81edf21a1a801ae07bef963cb7cd3b5ce700
Pragun Ananda <pragun.ananda@gmail.com>
2021-09-03 19:42:44 -0400

    Finish CRUD operations

--- backend/api.go
+++ backend/api.go
@@ -5,6 +5,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -31,12 +32,12 @@ func populateEntries() {
 	}
 }
 
-func returnAllEntries(w http.ResponseWriter, r *http.Request) {
+func getAllEntries(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Entries)
 }
 
-func returnSingleEntry(w http.ResponseWriter, r *http.Request) {
+func getSingleEntry(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
 
@@ -47,15 +48,45 @@ func returnSingleEntry(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createNewEntry(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "%+v", string(reqBody))
+
+	var entry Entry
+	json.Unmarshal(reqBody, &entry)
+	Entries = append(Entries, entry)
+	json.NewEncoder(w).Encode(entry)
+}
+
+func deleteEntry(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, entry := range Entries {
+		if entry.Id == id {
+			Entries = append(Entries[:index], Entries[index+1:]...)
+		}
+	}
+}
+
+func updateEntry(w http.ResponseWriter, r *http.Request) {
+	deleteEntry(w, r)
+	createNewEntry(w, r)
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/all", returnAllEntries)
-	http.HandleFunc("/entries/{}", returnSingleEntry)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", getAllEntries).Methods("GET")
+	myRouter.HandleFunc("/entries/{id}", getSingleEntry).Methods("GET")
+	myRouter.HandleFunc("/entries", createNewEntry).Methods("POST")
+	myRouter.HandleFunc("/entries/{id}", deleteEntry).Methods("DELETE")
+	myRouter.HandleFunc("/entries/{id}", updateEntry).Methods("UPDATE")
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
